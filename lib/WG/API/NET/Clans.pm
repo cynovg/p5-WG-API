e35aeef9f6b740e0c0d7701a24a96e87e3ab7cac
Cyrill Novgorodcev <cynovg@gmail.com>
2017-05-24 23:32:22 +0300

    fix work with params for NET/Clans

--- lib/WG/API/NET/Clans.pm
+++ lib/WG/API/NET/Clans.pm
@@ -5,41 +5,41 @@ use Moo::Role;
 our $VERSION = 'v0.8.1';
 
 sub clans_list {
-    my ( $self, $params ) = @_;
+    my $self = shift;
 
-    $self->_request( 'get', 'clans/list', ['language', 'fields', 'search', 'orded_by', 'limit', 'page_no' ], undef, $params );
+    $self->_request( 'get', 'clans/list', ['language', 'fields', 'search', 'orded_by', 'limit', 'page_no', 'game' ], undef, @_ );
 
     return $self->status eq 'ok' ? $self->response : undef;
 }
 
 sub clans_info {
-    my ( $self, $params ) = @_;
+    my $self = shift;
 
-    $self->_request( 'get', 'clans/info', ['language', 'fields', 'access_token', 'clan_id'], ['clan_id'], $params );
+    $self->_request( 'get', 'clans/info', ['language', 'fields', 'access_token', 'clan_id'], ['clan_id'], @_ );
 
     return $self->status eq 'ok' ? $self->response : undef; 
 }
 
 sub clans_membersinfo {
-    my ( $self, $params ) = @_;
+    my $self = shift;
 
-    $self->_request( 'get', 'clans/membersinfo', ['language', 'fields', 'account_id'], ['account_id'], $params );
+    $self->_request( 'get', 'clans/membersinfo', ['language', 'fields', 'account_id'], ['account_id'], @_ );
 
     return $self->status eq 'ok' ? $self->response : undef;
 }
 
 sub clans_glossary {
-    my ( $self, $params ) = @_;
+    my $self = shift;
 
-    $self->_request( 'get', 'clans/glossary', ['language', 'fields'], undef, $params );
+    $self->_request( 'get', 'clans/glossary', ['language', 'fields'], undef, @_ );
 
     return $self->status eq 'ok' ? $self->response : undef;
 }
 
 sub clans_messageboard {
-    my ( $self, %params ) = @_;
+    my $self = shift;
 
-    $self->_request( 'get', 'clans/mesageboard', ['language', 'fields', 'access_token'], ['access_token'], %params );
+    $self->_request( 'get', 'clans/mesageboard', ['language', 'fields', 'access_token'], ['access_token'], @_ );
 
     return $self->status eq 'ok' ? $self->response : undef;
 }
