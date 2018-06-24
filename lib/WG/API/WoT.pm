b21599f162e55e52ad645bf3d2d0b33cc679d23b
Cyrill Novgorodcev <cynovg@gmail.com>
2018-06-24 15:50:09 +0300

    fix POD

--- lib/WG/API/WoT.pm
+++ lib/WG/API/WoT.pm
@@ -336,6 +336,8 @@ sub tanks_achievements {
     );
 }
 
+=back
+
 =head1 BUGS
 
 Please report any bugs or feature requests to C<cynovg at cpan.org>, or through the web interface at L<https://gitlab.com/cynovg/WG-API/issues>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
